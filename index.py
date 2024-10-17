2979950c2f72fd7bcecd5e3e5d5e68ae92e09264
Wago25 <grivollet@laposte.net>
2024-10-17 16:23:29 +0200

    Avancement des class

--- index.py
+++ index.py
@@ -1,23 +1,40 @@
 from Couleurs import *
 
 
-print( colors.fg.green, "Amartya", colors.bg.red, "apagnan", colors.reset, "apagna")
+print( colors.fg.green, "Amartya")
 print( colors.fg.blue, "Amartya")
 print( colors.fg.red, "Amartya")
 print( colors.fg.yellow, "Amartya")
 
 
-print(colors.fg.lightgrey)
+print(colors.reset)
 
 class Blokus:
     
-    def __init__(self):
-        pass
+    def __init__(self,nbJoueur):
+        self.joueur = nbJoueur
+        
+
 
     def addPiece(aPiece):
         pass
+
+    def initPlateau():
+        pass
+
+    def printPLateau(plateau):
+        pass
+
+    def tour(joueur):
+        pass
+
 
 
 class piece(Blokus):
     def __init__(self, color, rotation, num):
         pass
+
+class joueur(Blokus):
+    def __init__(self, color, tab_piece):
+        pass
+
